a03c81420e068403375ffa626e939a50f3cca2a3
j9972 <j9972@naver.com>
2020-11-20 18:40:19 +0900

    id를 따서 js에 변수를 만들고 score부분 수정

---
+++ EscapingGame/Escaping.js
@@ -0,0 +1,4 @@
+const score = document.querySelector('#score');
+const start = document.querySelector('#start');
+const pause = document.querySelector('#pause');
+const store = document.querySelector('#store');
