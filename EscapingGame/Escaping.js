c6423b7829a9ee564151512aef8a4e39baabfc2f
j9972 <j9972@naver.com>
2020-11-21 18:57:11 +0900

    이동 하는 함수 이벤트 마저 만들기

--- EscapingGame/Escaping.js
+++ EscapingGame/Escaping.js
@@ -1,4 +1,29 @@
+const person = document.querySelector('#person');
+
+window.addEventListener('keydown', (e) => {
+    switch(e.code) {
+        case 'ArrowLeft': {
+            
+            break;
+        }   
+        case 'ArrowRight': {
+            break;
+        }
+    }
+});
+
 const score = document.querySelector('#score');
 const start = document.querySelector('#start');
 const pause = document.querySelector('#pause');
 const store = document.querySelector('#store');
+
+document.querySelector('#pause').addEventListener('click', () => {
+    // clearInterval(int);
+})
+document.querySelector('#start').addEventListener('click',() => {
+    // if(int) {
+    //     clearInterval(int);
+    // }else {
+    //     int = setInterval(tick,2000);
+    // }
+});
